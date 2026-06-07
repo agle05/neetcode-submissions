d417aafd93743942878a00d6dff38069764dcdd3
Aidan <154944072+agle05@users.noreply.github.com>
2026-06-07 17:45:52 +1200

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)-1):
+            complement = -(numbers[i]-target)
+            if complement in numbers and complement != numbers[i]:
+                left, right = min(i, numbers.index(complement)), max(i, numbers.index(complement))
+                return [left + 1, right + 1]
